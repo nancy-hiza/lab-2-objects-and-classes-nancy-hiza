ff81d7d90c312bdee970243bb7f667a9e816669a
nancy-hiza <hizanancy@gmail.com>
2022-05-24 11:57:54 +0300

    2020-04-02335 HIZA, NANCY

---
+++ src/com/Nancy/Main.java
@@ -0,0 +1,14 @@
+package com.Nancy;
+public class Main {
+     public static void main(String[] args) {
+
+         pencil Pencil1 = new pencil();
+         Pencil1.color = "Blue";
+         Pencil1.length = 12;
+         Pencil1.price = 250.0;
+
+         Pencil1.display();
+     }
+ }
+
+
---
+++ src/com/Nancy/pencil.java
@@ -0,0 +1,18 @@
+package com.Nancy;
+
+public class pencil {
+        String color;
+        int length;
+        double price;
+        public void display() {
+            System.out.println("The color of the pencil: " +color);
+            System.out.println("The length of the pencil : " +length);
+            System.out.println("The price of the pencil:Tsh " +price);
+
+        }
+
+    }
+
+
+
+
